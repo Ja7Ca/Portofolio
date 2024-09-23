730757d8f278442c708ea07d2b4f8bfc0d53313b
Ja7Ca <jarotsetiawan2503@gmail.com>
2024-09-23 22:25:04 +0700

    fix line loading

--- src/component/Loading/Section.tsx
+++ src/component/Loading/Section.tsx
@@ -37,7 +37,7 @@ const LoadingSection = () => {
         <div className={`panel w-full h-full bg-[white] overflow-hidden flex items-center justify-end transition-transform duration-1000 translate-x-[${isLoading ? "0%" : "-100%"}]`}>
             <p className={`text-[#272727] px-2 font-semibold duration-500 translate-x-[${text ? "0%" : "100%"}]`}>Jarot</p>
         </div>
-        <div className={`${panel ? "h-[6em]" : "h-[0em]"} absolute transition-transform duration-1000 w-[1px] text-[1vh] bg-[#272727]`}></div>
+        <div className={`${panel ? "h-[6em]" : "h-[0em]"} absolute transition-all duration-1000 w-[1px] text-[1vh] bg-[#272727]`}></div>
         <div className={`panel w-full h-full bg-[white] overflow-hidden flex items-center justify-start transition-transform duration-1000 translate-x-[${isLoading ? "0%" : "100%"}]`}>
             <p className={`text-[#272727] px-2 font-semibold duration-500 translate-x-[${text ? "0%" : "-100%"}]`}>Setiawan</p>
         </div>
